cb98e8b80842d802b1990444ca4ee2ab25b902eb
yyj <yyj6666667@gmail.com>
2026-03-01 15:56:59 +0800

    feat: add memory html

--- download_res.sh
+++ download_res.sh
@@ -5,6 +5,10 @@ gcloud storage cp /home/yyj/fiddler/benchmarks/vs_output/mixtral-offloading_prof
 
 gcloud storage cp /home/yyj/fiddler/benchmarks/vs_output/fiddler_profiler_trace_no_stack.json gs://856356105879-us-central1-blueprint-config/
 
+gcloud storage cp /home/yyj/fiddler/benchmarks/vs_output/mixtral-offloading_memory_timeline.html gs://856356105879-us-central1-blueprint-config/
+
+gcloud storage cp /home/yyj/fiddler/benchmarks/vs_output/fiddler_memory_timeline.html gs://856356105879-us-central1-blueprint-config/
+
 gcloud storage cp /home/yyj/fiddler/benchmarks/vs_output/latency.txt gs://856356105879-us-central1-blueprint-config/
 
 
@@ -12,6 +16,10 @@ gcloud storage cp /home/yyj/fiddler/benchmarks/vs_output/latency.txt gs://856356
 gcloud storage cp gs://856356105879-us-central1-blueprint-config/mixtral-offloading_profiler_trace_no_stack.json ./
 # on local
 gcloud storage cp gs://856356105879-us-central1-blueprint-config/fiddler_profiler_trace_no_stack.json ./
+# on local
+gcloud storage cp gs://856356105879-us-central1-blueprint-config/mixtral-offloading_memory_timeline.html ./
+# on local
+gcloud storage cp gs://856356105879-us-central1-blueprint-config/fiddler_memory_timeline.html ./
 # on local
 gcloud storage cp gs://856356105879-us-central1-blueprint-config/latency.txt ./
 
